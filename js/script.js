c80a8282de4a49e6f6bd2be97398a335e3c1fe47
Allan <allannicolas1912@gmail.com>
2026-04-21 07:55:33 -0300

    Estrutura inicial

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Script principal do projeto
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Página carregada com sucesso!');
+    
+    // Aqui você pode adicionar suas funções JavaScript
+});
